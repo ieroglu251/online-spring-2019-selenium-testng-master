478b9ed69e3655ba3ef708ecf42e79ca2a742338
ieroglu251 <ieroglu251@gmail.com>
2019-08-25 21:13:04 -0500

    selenium-testng

---
+++ src/test/java/com/cybertek/tests/day3/FirstAutomationScript.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstAutomationScript {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com");
+
+
+
+    }
+}
